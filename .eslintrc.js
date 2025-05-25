cf834265d67ddf65a38457d1536877c0e2b4454b
Zain <zain.abidin@twinciti.com>
2025-05-25 23:48:04 +0500

    chore: add .gitignore and setup Node.js backend environment

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    node: true,
+    es2020: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': ['error', {
+      semi: true,
+      singleQuote: true,
+      trailingComma: 'es5',
+      printWidth: 80,
+      tabWidth: 2,
+      bracketSpacing: true,
+      arrowParens: 'always',
+      endOfLine: 'auto',
+    }],
+    // You can add or override other rules here
+  },
+};

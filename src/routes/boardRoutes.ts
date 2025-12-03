c5b63081c574de68f657f1f78cbe9410f2cb9292
Zain <zainofficial258@gmail.com>
2025-12-04 00:50:18 +0500

    fix: remove redundant endpoints

--- src/routes/boardRoutes.ts
+++ src/routes/boardRoutes.ts
@@ -2,7 +2,6 @@
 import express from 'express';
 import { createBoard, getBoards, updateBoard, deleteBoard } from '../controllers/boardController';
 import { protect } from '../middlewares/authMiddleware';
-import { getBoardWithTasks } from '../controllers/getBoardWithTasks';
 import { isBoardOwnerOrAdmin } from '../middlewares/role';
 import { getBoardById } from '../controllers/boardController';
 
@@ -10,7 +9,6 @@ const router = express.Router();
 
 router.post('/', protect, createBoard);
 router.get('/', protect, getBoards);
-router.get("/:id/details", protect, getBoardWithTasks);
 router.get('/:boardId', protect, getBoardById);
 router.patch('/:id', protect, updateBoard);
 router.delete('/:id', protect, isBoardOwnerOrAdmin, deleteBoard);
